f3355a0094564aeeae2349756e522afce5028e15
OierGman <75170798+OierGman@users.noreply.github.com>
2022-11-22 23:03:11 +0000

    Quiz object creationa nd storage

---
+++ TmLms/Quiz.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TmLms
+{
+    internal class Quiz
+    {
+        public static List<Quiz> questions = new List<Quiz>();
+        public string ModuleCode { get; set; }
+        public string Type { get; set; }
+        public string Question { get; set; }
+        public string Correct_Ans { get; set; }
+        public List<string> Incorrect_Ans { get; set; }
+
+        public Quiz(string moduleCode, string type, string question, string correct_Ans, List<string> incorrect_Ans)
+        {
+            ModuleCode = moduleCode;
+            Type = type;
+            Question = question;
+            Correct_Ans = correct_Ans;
+            Incorrect_Ans = incorrect_Ans;
+        }
+    }
+}
